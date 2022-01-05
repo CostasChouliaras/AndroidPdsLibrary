1f49729b754bfffa0a9b8cb913643fc1807b00c4
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-01-05 09:27:56 +0200

    Refactoring

--- app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
+++ app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
@@ -10,37 +10,26 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.educationperfect.androidpdslibrary.ui.theme.AndroidPdsLibraryTheme
+import com.educationperfect.androidpdslibrary.*
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidPdsLibraryTheme {
-                // A surface container using the 'background' color from the theme
+               // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
-                    
 
                 }
             }
-        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidPdsLibraryTheme {
-        Greeting("Android")
-    }
 }
